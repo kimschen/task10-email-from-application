05cea0e10a2a19587ccbda93387f45e16b640dfc
kimwsc <KimWong@Hsiens-MacBook-Pro.local>
2017-03-30 15:13:08 +0800

    Code upload from local desktop

---
+++ Default.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="Default.aspx.vb" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="Style/StyleSheet.css" rel="stylesheet" type="text/css"/>
+    <script src="Scripts/jquery-2.1.4.min.js"></script>
+    </head>
+<body>
+    <form id="form1" runat="server">
+    <div id ="emailForm" runat="server">
+        <h1>Send Mail</h1>
+        <p><b>From :</b></p>
+        <p><asp:TextBox ID="txtmailfrom" runat="server" /></p>
+        <p><b>To :</b></p>
+        <p><asp:TextBox ID="txtmailto" runat="server" /></p>
+        <p><b>Subject :</b></p>
+        <p><asp:TextBox ID="txtmailsubject"  runat="server" /></p>
+        <p><b>Body :</b></p>
+        <p>
+            <asp:TextBox ID="txtText" textmode="MultiLine" Columns="20" Rows="20" runat="server" Height="202px" Width="556px" />
+        </p>
+        <p>Select your message priority:</p>
+        <p>
+            <asp:DropDownList ID="msgPriority" runat="server" Width="150px">
+                <asp:ListItem>Low</asp:ListItem>
+                <asp:ListItem>Normal</asp:ListItem>
+                <asp:ListItem>High</asp:ListItem>
+            </asp:DropDownList>
+        </p>
+        <p><asp:Button text="Send" OnClick="Button_Click" runat="server" Height="50px" Width="150px" /></p>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.vb
@@ -0,0 +1,47 @@
+﻿Imports System.Net.Mail
+
+Partial Class _Default
+    Inherits System.Web.UI.Page
+
+
+    Protected Sub Button_Click(sender As Object, e As EventArgs)
+
+        Dim mMailMessage As New MailMessage()
+
+        With mMailMessage
+            .From = New MailAddress(txtmailfrom.Text)
+            .To.Add(txtmailto.Text)
+            .Subject = txtmailsubject.Text
+            .BodyEncoding = Encoding.Default
+            .IsBodyHtml = True
+            .Body = "<center><font color:#336699;>HELLO!</font></center>"
+        End With
+
+        Dim plainView As AlternateView = AlternateView.CreateAlternateViewFromString(txtText.Text.ToString(), Nothing, "text/plain")
+        plainView.TransferEncoding = Net.Mime.TransferEncoding.QuotedPrintable
+
+        Dim htmlView As AlternateView = AlternateView.CreateAlternateViewFromString(txtText.Text.ToString(), Nothing, "text/html")
+        htmlView.TransferEncoding = Net.Mime.TransferEncoding.QuotedPrintable
+
+        mMailMessage.AlternateViews.Add(plainView)
+        mMailMessage.AlternateViews.Add(htmlView)
+
+        Select Case msgPriority.SelectedValue
+            Case "Low"
+                mMailMessage.Priority = MailPriority.Low
+            Case "Normal"
+                mMailMessage.Priority = MailPriority.Normal
+            Case "High"
+                mMailMessage.Priority = MailPriority.High
+        End Select
+
+        Dim mailClient As New SmtpClient
+        mailClient.Send(mMailMessage)
+        mailClient.Dispose()
+
+        emailForm.Visible = False
+        Response.Write("Thanks for your email!")
+
+    End Sub
+
+End Class
